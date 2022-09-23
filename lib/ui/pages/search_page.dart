a7be4c04630e8aa7b44a3be320da319f0660a351
Azamat <aza@MacBook-Pro-Azamat.local>
2022-09-23 19:07:36 +0600

    end project

--- lib/ui/pages/search_page.dart
+++ lib/ui/pages/search_page.dart
@@ -1,10 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:r_and_m/bloc/character_bloc.dart';
+import 'package:r_and_m/data/models/character.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
 
   @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  late Character _currentCharacter;
+  List<Results> _currentResults = [];
+  int _currentPage = 1;
+  String _currentSearchStr = '';
+
+  @override
+  void initState() {
+    if (_currentResults.isEmpty) {
+      context
+          .read<CharacterBloc>()
+          .add(const CharacterEventFetch(name: '', page: 1));
+    }
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    final state = context.watch<CharacterBloc>().state;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding:
+              const EdgeInsets.only(top: 15, bottom: 1, right: 16, left: 16),
+          child: TextField(
+            style: const TextStyle(color: Colors.white),
+            cursorColor: Colors.white,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: const Color.fromRGBO(86, 86, 86, 0.8),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide.none,
+              ),
+              prefixIcon: const Icon(Icons.search, color: Colors.white),
+              hintText: 'Search name',
+              hintStyle: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            onChanged: (value) {
+              _currentPage = 1;
+              _currentResults = [];
+              _currentSearchStr = value;
+              context
+                  .read<CharacterBloc>()
+                  .add(CharacterEvent.fetch(name: value, page: _currentPage));
+            },
+          ),
+        ),
+        Expanded(
+          child: state.when(
+            loading: () {
+              return Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    CircularProgressIndicator(
+                      strokeWidth: 2,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('Loading...'),
+                  ],
+                ),
+              );
+            },
+            loaded: (characterLoaded) {
+              _currentCharacter = characterLoaded;
+              _currentResults = _currentCharacter.results;
+              return _currentResults.isNotEmpty
+                  ? Text('$_currentResults')
+                  : const SizedBox();
+            },
+            error: () => const Text('Nothing found...'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _customListView(List<Results> currentResults) {
+    return ListView.separated(
+      separatorBuilder: (_, index) => const SizedBox(
+        height: 5,
+      ),
+      itemCount: currentResults.length,
+      shrinkWrap: true,
+      itemBuilder: (context, index){
+
+      },
+    );
   }
 }
